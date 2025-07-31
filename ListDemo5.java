293776b11a6ebede0c1f0d8438ea55156298f237
Ujjawal Dubey <udubey938@gmail.com>
2025-07-31 07:07:34 +0530

    Add files via upload

---
+++ ListDemo5.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+public class ListDemo5
+{
+
+	public static void main(String[] args) {
+		ArrayList<String> List5=new ArrayList<String>();
+		List5.add("Arun");
+	    List5.add("Anuj");
+		List5.add("Arpit");
+		//Checking list5 is empty or not
+		System.out.println("[LIST 5]="+List5.isEmpty());
+		//creating new list6
+		ArrayList<String> List6=new ArrayList<String>();
+		System.out.println("[LIST 6]="+List6.isEmpty());
+		
+	 }
+
+}
