1fd0edea9b3fa5488e0eadd1a00d99c46a25aab3
Ujjawal Dubey <udubey938@gmail.com>
2025-08-01 07:51:38 +0530

    Add files via upload

---
+++ ListDemo6.java
@@ -0,0 +1,15 @@
+
+public class ListDemo6
+{
+
+	public static void main(String[] args)
+	{
+		String[] arr={"Alok","Ayush","Anuj","Aryan"};
+		for(String name:arr)
+		{
+			System.out.println(name);
+		}
+
+	}
+
+}
