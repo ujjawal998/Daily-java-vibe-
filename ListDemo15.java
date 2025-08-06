b18377d32ed132b94bc2f769f4068cf4f681a4fb
Ujjawal Dubey <udubey938@gmail.com>
2025-08-06 09:05:38 +0530

    Add files via upload

---
+++ ListDemo15.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.util.List;
+public class ListDemo15
+{
+	public static void main(String[] args)
+	{
+		List<String> List15=new ArrayList<String>();
+		List15.add("Arun");
+		List15.add("Rohit");
+		List15.add("Anuj");
+		List15.add("Ankit");
+		List15.add("Ujjawal");
+		//Getting element by get method with the help of index
+		System.out.println("[Getting Element by Get method]="+List15.get(2));
+		System.out.println("[Getting Element by Get method]="+List15.get(1));
+		System.out.println("[Getting element by Get method]="+List15.get(4));
+	
+
+	}
+
+	
+}
