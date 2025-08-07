d23c7afe9eba165e0fbf19a495266a88e9558ab3
Ujjawal Dubey <udubey938@gmail.com>
2025-08-07 08:08:16 +0530

    Add files via upload

---
+++ ListDemo16.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.util.List;
+public class ListDemo16
+{
+	public static void main(String[] args)
+	{
+		List<String> List16=new ArrayList<String>();
+		List16.add("Aman");
+		List16.add("Anurag");
+		List16.add("Rohit");
+		List16.add("Ujjawal");
+	    // before implementation set method
+		System.out.println("Before implementing set method LIST 16="+List16);
+		//after implementation of set method
+		List16.set(0,"Anuj");
+		System.out.println("[After implementation of set method LIST 16]="+List16);
+		
+		}
+
+	
+	
+}
