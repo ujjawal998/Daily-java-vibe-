eca1a097d283c9678a8c7c946c64e2db7a482d10
Ujjawal Dubey <udubey938@gmail.com>
2025-08-09 07:23:09 +0530

    Add files via upload

---
+++ TreeDemoMap.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class TreeDemoMap
+{
+	public static void main(String[] args)
+	{
+		Map<String,String> map=new TreeMap<String,String>();
+		map.put("Aman","923724620");
+		map.put("Rohit","27943719");
+		map.put("Anuj","807426496");
+		map.put("Anurag","23729642");
+		// Overwrites the previous value for "Aman"
+		map.put("Aman","1936193619");
+		map.put("David","3793791047");
+		System.out.println(map);
+		
+	}
+
+}
