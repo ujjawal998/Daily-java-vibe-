42e9b2818b31c57f742365f007a7ea3f96019655
Ujjawal Dubey <udubey938@gmail.com>
2025-08-10 07:37:37 +0530

    Add files via upload

---
+++ TreesetDemo.java
@@ -0,0 +1,21 @@
+import java.util.TreeSet;
+import java.util.Set;
+public class TreesetDemo
+{
+
+	public static void main(String[] args) 
+	{
+		Set<String> Demo=new TreeSet<String>();
+		Demo.add("AA");
+		Demo.add("CC");
+		Demo.add("BB");
+		Demo.add("KK");
+		Demo.add("YY");
+		Demo.add("QQ");
+		Demo.add("DD");
+		Demo.add("UU");
+		System.out.println(Demo);
+		
+	}
+
+}
