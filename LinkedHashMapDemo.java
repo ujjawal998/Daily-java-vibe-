bbe7cdc02a1bce1455792e0e63000a079a56b5cd
Ujjawal Dubey <udubey938@gmail.com>
2025-08-18 21:20:32 +0530

    Add files via upload

---
+++ LinkedHashMapDemo.java
@@ -0,0 +1,17 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+public class LinkedHashMapDemo
+{
+	public static void main(String[] args)
+	{
+		Map<String,String> map=new LinkedHashMap<String,String>();
+		map.put("Aman","923724620");
+		map.put("Rohit","27943719");
+		map.put("Anuj","807426496");
+		map.put("Anurag","23729642");
+		map.put("David","3793791047");
+		System.out.println(map);
+		
+	}
+
+}
