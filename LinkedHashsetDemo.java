e795c399fe9839e6d6530873406d8b5be4e4bef3
Ujjawal Dubey <udubey938@gmail.com>
2025-08-20 08:09:20 +0530

    Add files via upload

---
+++ LinkedHashsetDemo.java
@@ -0,0 +1,22 @@
+import java.util.LinkedHashSet;
+import java.util.*;
+import java.util.Set;
+public class LinkedHashsetDemo
+{
+
+	public static void main(String[] args) 
+	{
+		Set<String> Demo=new LinkedHashSet<String>();
+		Demo.add("AA");
+		Demo.add("CC");
+		Demo.add("BB");
+		Demo.add("KK");
+		Demo.add("YY");
+		Demo.add("QQ");
+		Demo.add("DD");
+		Demo.add("UU");
+		System.out.println(Demo);
+		
+		}
+
+}
