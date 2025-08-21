f7b164fd55e73d5aaaeb1307de93399ba55b0dc4
Ujjawal Dubey <udubey938@gmail.com>
2025-08-21 06:37:57 +0530

    Add files via upload

---
+++ HashSetDemo.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class HashSetDemo
+{
+
+	public static void main(String[] args) 
+	{
+		Set<String> Demo=new HashSet<String>();
+		Demo.add("AA");
+		Demo.add("CC");
+		Demo.add("BB");
+		Demo.add("KK");
+		Demo.add("YY");
+		Demo.add("QQ");
+		Demo.add("DD");
+		Demo.add("UU");
+		Demo.add("AA");
+		System.out.println(Demo);
+		
+		}
+
+}
