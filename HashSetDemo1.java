21fabed9d093dbf66edd2a386aea9cb125ef0e21
Ujjawal Dubey <udubey938@gmail.com>
2025-08-22 09:14:47 +0530

    Add files via upload

---
+++ HashSetDemo1.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.*;
+public class HashSetDemo1 
+{
+	public static void main(String[] args)
+	{
+		Set<Integer> Set1=new HashSet<Integer>();
+		Set1.add(10);
+		Set1.add(23);
+		Set1.add(32);
+		Set1.add(15);
+		Set1.add(3);
+		Set1.add(16);
+		Set1.add(1);
+		Set1.add(5);
+		System.out.println(Set1);
+		
+		}
+
+}
