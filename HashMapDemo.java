517ead73c0c55c252e645937f56209ab4fedec45
Ujjawal Dubey <udubey938@gmail.com>
2025-08-23 07:37:58 +0530

    Add files via upload

---
+++ HashMapDemo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class HashMapDemo
+{
+	public static void main(String[] args)
+	{
+		Map<String,String> map=new HashMap<String,String>();
+		map.put("Aman","923724620");
+		map.put("Rohit","27943719");
+		map.put("Anuj","807426496");
+		map.put("Anurag","23729642");
+		map.put("david","3793791047");
+		System.out.println(map);
+		
+	}
+
+}
