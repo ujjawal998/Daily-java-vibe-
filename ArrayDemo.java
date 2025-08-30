a31fda9251d49b050bdf4a9d6e8f48aa100d0fc1
Ujjawal Dubey <udubey938@gmail.com>
2025-08-30 11:03:35 +0530

    Add files via upload

---
+++ ArrayDemo.java
@@ -0,0 +1,19 @@
+
+public class ArrayDemo
+{
+
+	public static void main(String[] args)
+	{
+		int [] arr1=new int[5];
+		boolean [] arr2=new boolean[5];
+		float [] arr3=new float[5];
+		char [] arr4=new char[5];
+		product [] arr5=new product[5];
+		System.out.println(arr1[2]);
+		System.out.println(arr2[4]);
+		System.out.println(arr3[4]);
+		System.out.println(arr4[4]);
+		System.out.println(arr5[4]);
+		
+	}
+}
